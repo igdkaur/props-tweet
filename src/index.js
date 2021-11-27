72bf0a3d5c1ccc45c5208fcd232a912b722b1593
igdkaur <igdkaur@gmail.com>
2021-11-27 20:40:49 +0530

    pass props to all components

--- src/index.js
+++ src/index.js
@@ -1,50 +1,85 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import moment from "moment";
 
-const Time = () => <span className="time">3h ago</span>;
-const ReplyButton = () => <i className="fa fa-reply reply-button" />;
-const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
-const LikeButton = () => <i className="fa fa-heart like-button" />;
-const MoreOptionsButton = () => (
-  <i className="fa fa-ellipsis-h more-options-button" />
-);
-
-function Avatar({ hash }) {
-  var url = `https://www.gravatar.com/avatar/${hash}`;
-  return (
-  <img
-  src={url}
-  className="avatar"
-  alt="avatar" />
-  );
-  }
-// add the testTweet object, which
-// will serve as our fake data
 var testTweet = {
   message: "Something about cats.",
   gravatar: "xyz",
   author: {
-  handle: "catperson",
-  name: "IAMA Cat Person"
+    handle: "catperson",
+    name: "IAMA Cat Person",
   },
   likes: 2,
-  retweets: 0,
-  timestamp: "2016-07-30 21:24:37"
-  }
+  retweets: 5,
+  timestamp: "2016-07-30 21:24:37",
+};
 
-function Tweet({tweet}) { //1. update the Tweet component to accept a tweet prop
+function getRetweetCount(count) {
+  if (count > 0) {
+    return <span className="retweet-count">{count}</span>;
+  } else {
+    return null;
+  }
+}
+
+const RetweetButton = ({ count }) => (
+  <span className="retweet-button">
+    <i className="fa fa-retweet" />
+    {getRetweetCount(count)}
+  </span>
+);
+const LikeButton = ({ count }) => (
+  <span className="like-button">
+    <i className="fa fa-heart" />
+    {count > 0 && <span className="like-count">{count}</span>}
+  </span>
+);
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+function NameWithHandle({ author }) {
+  const { name, handle } = author;
+  return (
+    <span className="name-with-handle">
+      <span className="name">{name}</span>
+      <span className="handle">@{handle}</span>
+    </span>
+  );
+}
+const Time = ({ time }) => {
+  const timeString = moment(time).fromNow();
+  return <span className="time">{timeString}</span>;
+};
+
+function Avatar({ hash }) {
+  var url = `https://www.gravatar.com/avatar/${hash}`;
+  return <img src={url} className="avatar" alt="avatar" />;
+}
+// add the testTweet object, which
+// will serve as our fake data
+
+function Message({ text }) {
+  return <div className="message">{text}</div>;
+}
+
+function Tweet({ tweet }) {
+  //1. update the Tweet component to accept a tweet prop
   return (
     <div className="tweet">
-      <Avatar hash={tweet.gravatar}/>
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <NameWithHandle />
-        <Time />
-        <Message />
+        <NameWithHandle author={tweet.author} />
+        <Time time={tweet.timestamp} />
+        <Message text={tweet.message} />
         <div className="buttons">
           <ReplyButton />
-          <RetweetButton />
-          <LikeButton />
+          <RetweetButton count={tweet.retweets} />
+          <LikeButton count={tweet.likes} />
           <MoreOptionsButton />
         </div>
       </div>
@@ -52,20 +87,7 @@ function Tweet({tweet}) { //1. update the Tweet component to accept a tweet prop
   );
 }
 
-function Message() {
-  return <div className="message">This is less than 150 characters.</div>;
-}
-
-function NameWithHandle() {
-  return (
-    <span className="name-with-handle">
-      <span className="name">Kapil</span>
-      <span className="handle">@yourKapil</span>
-    </span>
-  );
-}
-
 // update the call to ReactDOM.render to pass
 // the testTweet object into the tweet prop.
 
-ReactDOM.render(<Tweet tweet = {testTweet} />, document.querySelector("#root"));
+ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector("#root"));
